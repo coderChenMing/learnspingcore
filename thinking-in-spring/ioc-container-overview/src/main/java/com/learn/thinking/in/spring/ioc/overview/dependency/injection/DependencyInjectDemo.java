40840a165ea9ed23279a3deb429a126bf79366ce
coderChenMing <280433706@qq.com>
2021-08-22 00:58:47 +0800

    依赖注入

--- thinking-in-spring/ioc-container-overview/src/main/java/com/learn/thinking/in/spring/ioc/overview/dependency/injection/DependencyInjectDemo.java
+++ thinking-in-spring/ioc-container-overview/src/main/java/com/learn/thinking/in/spring/ioc/overview/dependency/injection/DependencyInjectDemo.java
@@ -14,11 +14,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * 依赖注入:
  * @date 2021/8/20 0020 0:50
  */
-public class DependencyLookUpDemo {
+public class DependencyInjectDemo {
     public static void main(String[] args) {
         // 创建上下文
         // 配置xml配置文件,启动上下文
-        BeanFactory beanFactory = new ClassPathXmlApplicationContext("classpath:/META-INF/dependency-lookup-context.xml");
+        BeanFactory beanFactory = new ClassPathXmlApplicationContext("classpath:/META-INF/dependency-injection-context.xml");
 
     }
 }
