ebd9464635d7969e7010c55781939e259d7445be
coderChenMing <280433706@qq.com>
2021-08-30 22:45:08 +0800

    spring容器分析

--- thinking-in-spring/ioc-container-overview/src/main/java/com/learn/thinking/in/spring/ioc/overview/dependency/injection/DependencyInjectDemo.java
+++ thinking-in-spring/ioc-container-overview/src/main/java/com/learn/thinking/in/spring/ioc/overview/dependency/injection/DependencyInjectDemo.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.env.Environment;
 
 /**
  * Project: thinking-in-spring
@@ -23,24 +24,39 @@ public class DependencyInjectDemo {
         // 创建上下文
         // 配置xml配置文件,启动上下文
         BeanFactory beanFactory = new ClassPathXmlApplicationContext("classpath:/META-INF/dependency-injection-context.xml");
-        UserRepository userRepository = beanFactory.getBean(UserRepository.class);
-        // userRepository.getUsers().forEach(System.out::println);
-        // 依赖注入,注入的beanFactory是内建bean
-        //System.out.println(userRepository.getBeanFactory());
-        //System.out.println(userRepository.getBeanFactory() == beanFactory);// false
-        // 依赖查找(下面这行会报错)
-        // System.out.println(beanFactory.getBean(BeanFactory.class)); // NoSuchBeanDefinitionException: No qualifying bean of type 'org.springframework.beans.factory.BeanFactory'
-        // 以上证明依赖查找和依赖注入并不是来自同源
+        // 自定义bean
+        UserRepository userRepository = beanFactory.getBean("useRepository", UserRepository.class);
+        // 证明依赖查找和依赖注入并不是来自同源
+         testBeanFactory(userRepository, beanFactory);
 
-
+        System.out.println("-------------------------------------------------------------------------");
         ObjectFactory<ApplicationContext> objectFactory = userRepository.getObjectFactory();
 
-        //org.springframework.context.support.ClassPathXmlApplicationContext@1a6c5a9e
+        // org.springframework.context.support.ClassPathXmlApplicationContext@1a6c5a9e
         System.out.println(objectFactory.getObject());
 
-        //true
-        System.out.println(objectFactory.getObject()==beanFactory);
+        // true
+        System.out.println(objectFactory.getObject() == beanFactory);
         // 总结 userRepository.getObjectFactory().getObject() == applicationContext != userRepository.getBeanFactory()
+
+        System.out.println(beanFactory.getBean(Environment.class));
     }
 
+    private static void testBeanFactory(UserRepository userRepository, BeanFactory beanFactory) {
+        //userRepository.getUsers().forEach(System.out::println);
+        // 依赖注入,注入的beanFactory是内建bean
+        // org.springframework.beans.factory.support.DefaultListableBeanFactory@3b81a1bc
+        System.out.println(userRepository.getBeanFactory());
+
+        //org.springframework.context.support.ClassPathXmlApplicationContext@1a6c5a9e
+        System.out.println(beanFactory);
+
+        // false
+        System.out.println(userRepository.getBeanFactory() == beanFactory);
+        // 依赖查找(下面这行会报错)
+        // BeanFactory.class:容器建立时使用的bean,beanFactory获取不到
+        // NoSuchBeanDefinitionException: No qualifying bean of type 'org.springframework.beans.factory.BeanFactory'
+        System.out.println(beanFactory.getBean(BeanFactory.class));
+
+    }
 }
